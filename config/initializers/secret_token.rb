e12e67a58967042c2901b88d9e82eff917b4db48
Scott Feinberg <scott@allout.org>
2014-02-02 18:50:17 -0500

    iniit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp::Application.config.secret_key_base = '7e38a349df140bf3fdf9c45a79b6bdf55601c48f074113b6128fff4dcae7c0965825c5d70aac3c62ef9a04e16af97ca4632beb66da8a8fdb40ea00f8958f6602'
